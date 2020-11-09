040840656b7d67a5d804b96e4107b0ef740ef828
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2020-11-09 16:45:20 +0100

    Complete exercise enum1

--- src/it/unibo/oop/lab/enum1/TestSportByEnumeration.java
+++ src/it/unibo/oop/lab/enum1/TestSportByEnumeration.java
@@ -1,5 +1,7 @@
 package it.unibo.oop.lab.enum1;
 
+import it.unibo.oop.lab.socialnetwork.User;
+
 /**
  * This is going to act as a test for
  * {@link it.unibo.oop.lab.enum1.SportSocialNetworkUserImpl}
@@ -24,6 +26,36 @@ public final class TestSportByEnumeration {
          * 
          * By now, you know how to do it
          */
-        // TODO
+        /*
+         * Create 3 users
+         */
+    	final SportSocialNetworkUserImpl<User> kelvin = new SportSocialNetworkUserImpl<>("Kelvin", "Olaiya", "kel$", 20);
+    	final SportSocialNetworkUserImpl<User> stefano = new SportSocialNetworkUserImpl<> ("Stefano", "Scolari", "scolapasta", 20);
+    	final SportSocialNetworkUserImpl<User> lore = new SportSocialNetworkUserImpl<>("Lorenzo", "Maiani", "lore", 21);
+    	
+    	/*
+    	 * Kelvin likes: BASKET, BIKE, VOLLEY
+    	 */
+    	kelvin.addSport(Sport.BASKET);
+    	kelvin.addSport(Sport.BIKE);
+    	kelvin.addSport(Sport.VOLLEY);
+    	/*
+    	 * Lorenzo likes: BASKET, BIKE
+    	 */
+    	lore.addSport(Sport.BASKET);
+    	lore.addSport(Sport.BIKE);
+    	/*
+    	 * Sefano likes: BIKE, SOCCER
+    	 */
+    	stefano.addSport(Sport.BIKE);
+    	stefano.addSport(Sport.SOCCER);
+    	/*
+    	 * test
+    	 */
+    	System.out.println("[TEST] Kelvin likes basket: " + kelvin.hasSport(Sport.BASKET));
+    	System.out.println("[TEST] Kelvin doesn't like F1: " + !kelvin.hasSport(Sport.F1));
+    	System.out.println("[TEST] Stefano likes bike: " + stefano.hasSport(Sport.BIKE));
+    	System.out.println("[TEST] Stefano doesn't like VOLLEY: " + !stefano.hasSport(Sport.VOLLEY));
+    	
     }
 }
